d0524b9449ba0d9507b977da0794aa6fed6a7b04
Nidhi <nidhi@auberginesolutions.com>
2020-04-30 19:18:30 +0530

    nav link styling

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from '../Posts/Posts';
-import {Route,Link} from 'react-router-dom';
+import {Route,NavLink} from 'react-router-dom';
 import NewPost from '../NewPost/NewPost';
 
 // import FullPost from '../../components/FullPost/FullPost';
@@ -17,19 +17,26 @@ class Blog extends Component {
                 <header  className="Blog">
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink 
+                            to="/" 
+                            exact
+                            activeClassName="active"
+                            activeStyle={{
+                                color: 'orange',
+                                textDecoration: 'underline'
+                            }}>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: 'new-posts',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                {/*  <Route path='/' exact render={()=><h1>Home</h1>}/>
                 <Route path='/' render={()=><h1>Home 2</h1>}/> */}
                 <Route path='/' exact component={Posts}/>
-                <Route path='/new-posts'component={NewPost}/>
+                <Route path='/new-posts' component={NewPost}/>
 
 
                 {/* <section>
