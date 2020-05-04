abc8cc727f6000bf938246609d66bac309b6bc11
Nidhi <nidhi@auberginesolutions.com>
2020-05-04 10:52:24 +0530

    laxy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from '../Posts/Posts';
 import { Route, NavLink, Switch,Redirect } from 'react-router-dom';
-import NewPost from '../NewPost/NewPost';
+//import NewPost from '../NewPost/NewPost';
+import asyncComponent from '../../hoc/ayncComponent'
 
 // import Post from '../../components/Post/Post';
 // import axios from 'axios';
 
-
+const AsynNewPost= asyncComponent(()=>{
+    return import('../NewPost/NewPost');
+})
 class Blog extends Component {
     state = {
         auth: false
@@ -39,7 +42,7 @@ class Blog extends Component {
                 <Route path='/' render={()=><h1>Home 2</h1>}/> */}
                 <Switch>
                     {/* {this.state.auth?<Route path='/new-posts' component={NewPost} />:null}  if you want to only render of authenticated */}
-                    <Route path='/new-posts' component={NewPost}/>
+                    <Route path='/new-posts' component={AsynNewPost}/>
                     <Route path='/posts' component={Posts} />
                     <Route render={()=><h1>Page Not Found</h1>}/>  {/*for 404 error*/}
                     <Redirect from='/' to='/posts'/>
