05000a47a673effbc5abb6e8157fcf3e9583dadd
Nidhi <nidhi@auberginesolutions.com>
2020-05-04 10:52:17 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      //  <BrowserRouter basename="/my-app"/> render app from some base directory
       <BrowserRouter>
         <div className="App">
           <Blog />
